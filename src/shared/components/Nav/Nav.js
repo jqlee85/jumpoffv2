3157ad1534918cda5f642a512c9b34806f2119b2
Jesse Lee <jqlee85@gmail.com>
2018-01-23 02:54:06 -0300

    nav styling

--- src/shared/components/Nav/Nav.js
+++ src/shared/components/Nav/Nav.js
@@ -6,15 +6,19 @@ import { Link } from 'react-router-dom';
 class Nav extends Component {
 
   render() {
+    
+    let list = null;
+    if (this.props.menuToggled) {
+      list = <ul className="menu">
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      </ul>;
+    }
+    
     let theClasses = 'main-nav';
     if (this.props.menuToggled) theClasses += ' toggled';
     return <nav id="main-nav" className={theClasses}>
-        
-        <ul className="menu">
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-        </ul>
-        
+      {list}
     </nav>;
   }
 
