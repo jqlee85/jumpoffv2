cb72d66b818b32b4bcef59847b7ead10e4f7df39
Jesse Lee <jqlee85@gmail.com>
2018-01-31 14:54:02 -0300

    working out svg logo

--- src/shared/components/HomeSectionOne/HomeSectionOne.js
+++ src/shared/components/HomeSectionOne/HomeSectionOne.js
@@ -10,7 +10,25 @@ class HomeSectionOne extends Component {
   render(){
     return <section className="jo-section home-section-one">
       <div className="jo-section-half first">
-        <h1>LOGO</h1>
+        <div className="jo-logo-featured-wrapper">
+          <div className="jo-logo-featured">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" 
+              width="100%"
+              height="100%"
+              x="0"
+              y="0"
+              viewBox="0 0 200 200"
+            >
+              <path 
+                
+                d="M 0 25 L 175 25 L 175 175 L 25 175 L 25 100"
+                fill="none"
+                stroke="black"
+                strokeWidth="50"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
       <div className="jo-section-half last">
         <h1>JumpOff</h1>
