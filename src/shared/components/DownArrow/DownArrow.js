39890303eeb40bce2d9bb2bb03963c11499533e8
Jesse Lee <jqlee85@gmail.com>
2018-05-24 03:01:29 -0400

    down arrow styling

--- src/shared/components/DownArrow/DownArrow.js
+++ src/shared/components/DownArrow/DownArrow.js
@@ -13,7 +13,7 @@ class DownArrow extends Component {
 
   render(){
     
-    let size = this.props.size || 1000;
+    let size = this.props.size || 60;
     let style = {
       width: size,
       height: size
@@ -21,8 +21,8 @@ class DownArrow extends Component {
     let color = this.props.color || 'black';
     let hoverColor = this.props.hoverColor || 'white';
 
-    return <a href="">
-      <svg className="jo-down-arrow" style={style} version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000">
+    return <a className="jo-down-arrow" href="">
+      <svg style={style} version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000">
         <g>
           <circle cx="500" cy="500" r="480" stroke={color} stroke-width="40" fill="none" />
           <path stroke={color} stroke-width="40" fill="transparent" d="M300 400 L500 620 L700 400"></path>
