8d4039dccf4708c62aa7995f94778bfdfa4c7cfe
Jesse Lee <jqlee85@gmail.com>
2018-07-08 21:47:59 -0400

    move app state from redux to react

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,15 +1,8 @@
 import styles from './App.css';
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 import { withRouter } from 'react-router'
 import routes from "./routes";
 import {PropsRoute} from 'react-router-with-props';
-import {connect} from 'react-redux';
-import {requestUserLogin} from './actions/userActions';
-import {userLogout} from './actions/userActions';
-import {toggleNav} from './actions/appActions';
-import {toggleNavFront} from './actions/appActions';
-import {toggleNavFade} from './actions/appActions';
 import Nav from './components/Nav/Nav';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -18,50 +11,62 @@ class App extends Component {
 
   constructor(props) {
       super(props);
+      this.state = { 
+        navToggled: false,
+        navFadeToggled: false,
+        navFrontToggled: false
+      };  
   }
 
   toggleAppNav = () => {
     // Fade/Unfade Nav Items
-    this.props.toggleNavFade();
+    this.setState(prevState => ({
+      navFadeToggled: !prevState.navFadeToggled
+    }));
     // Change Navigation Z-Index
-    if (this.props.app.navFront) {
+    if (this.state.navFrontToggled) {
       setTimeout(() => {
-        this.props.toggleNavFront();
+        this.setState(prevState => ({
+          navFrontToggled: !prevState.navFrontToggled
+        }));
       },400);
     } else {
-      this.props.toggleNavFront();
+      this.setState(prevState => ({
+        navFrontToggled: !prevState.navFrontToggled
+      }));
     }
     // Toggle the Menu Opacity
     setTimeout(() => {
-      this.props.toggle
-      this.props.toggleNav();
+      this.setState(prevState => ({
+        navFadeToggled: !prevState.navFadeToggled
+      }));
+      this.setState(prevState => ({
+        navToggled: !prevState.navToggled
+      }));
     }, 100); 
   }
 
-  // toggleNav = () => {
-  //   this.props.app.navToggled = !this.props.app.navToggled;
-  // }
+  toggleNav
 
   render(){
     
     let appClasses = 'App';
-    if (this.props.app.menuToggled) appClasses += ' app-menu-toggled';
-    if (this.props.app.navFadeToggled) appClasses += ' nav-fade';
+    if (this.state.navToggled) appClasses += ' app-menu-toggled';
+    if (this.state.navFadeToggled) appClasses += ' nav-fade';
     return <div id="App" className={appClasses}>
       <Nav 
-        menuToggled={this.props.app.menuToggled} 
+        menuToggled={this.state.navToggled} 
         userLogin={this.props.requestUserLogin} 
         userData={this.props.user} 
         userLogout={this.props.userLogout}
         toggleNav={this.toggleAppNav}
-        navFront={this.props.app.navFront}
+        navFront={this.state.navFrontToggled}
       />
       <Header 
-        menuToggled={this.props.app.menuToggled} 
+        menuToggled={this.state.navToggled} 
         userLogin={this.props.requestUserLogin} 
         userData={this.props.user}
         toggleNav={this.toggleAppNav}
-        // toggleNavNonToggledMenu={}
       />
       <div className="main">
         {routes.map((route, i) => <PropsRoute 
@@ -69,11 +74,6 @@ class App extends Component {
           exact={route.exact} 
           path={route.path} 
           component={route.component} 
-          user={this.props.user}
-          blog={this.props.blog}
-          app={this.props.app}
-          fetchBlogPost={this.props.fetchBlogPost}
-          updateCurrentSection={this.props.updateCurrentSection}
         />)}
       </div>
       <Footer />
@@ -82,24 +82,5 @@ class App extends Component {
 
 }
 
-// Application State
-const mapStateToProps = (state) => {
-  return {
-    app: state.app,
-    user: state.user
-  }
-}
-const mapDispatchToProps = (dispatch) => {
-  return {
-    toggleNav: (data) => {
-      dispatch(toggleNav(data));
-    },
-    toggleNavFade: (data) => {
-      dispatch(toggleNavFade(data));
-    },
-    toggleNavFront: (data) => {
-      dispatch(toggleNavFront(data));
-    }
-  }
-}
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
+
+export default withRouter(App);
--- src/shared/configureStore.js
+++ src/shared/configureStore.js
@@ -1,9 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { createLogger } from 'redux-logger';
-import app from "./reducers/appReducer";
 import user from "./reducers/userReducer";
-
 const env = process.env.NODE_ENV || 'development';
 const middleware = [thunk];
 const logger = createLogger();
@@ -13,7 +11,6 @@ if (env === 'development') middleware.push(logger);
 const configureStore = preloadedState =>
   createStore(
     combineReducers({
-      app,
       user
     }), 
     preloadedState, 
